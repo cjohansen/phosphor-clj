5ed952c3306207da34ecb5455d74f6e286c07e1e
Christian Johansen <christian@cjohansen.no>
2023-04-20 23:46:20 +0200

    Separate ingest code to avoid hickory dependency in production

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ target/phosphor-icons: target/phosphor-icons.zip
 	unzip -o target/phosphor-icons.zip -d target/phosphor-icons
 
 update-icons: target/phosphor-icons
-	clojure -M:dev -m phosphor.icons target/phosphor-icons/2.0.0/SVGs
+	clojure -M:dev -m phosphor.ingest target/phosphor-icons/2.0.0/SVGs
 
 phosphor.jar: src/phosphor/*
 	rm -f phosphor.jar && clojure -A:dev -M:jar
--- src/phosphor/icons.clj
+++ src/phosphor/icons.clj
@@ -1,9 +1,6 @@
 (ns phosphor.icons
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [hickory.core :as hiccup]))
-
-(render :phosphor.regular/x)
+            [clojure.string :as str]))
 
 (def base-path "phosphor-icons/2.0.0")
 
@@ -55,35 +52,3 @@
          'do
          (for [id (get-icon-ids*)]
            (list 'phosphor.icons/icon id))))
-
-(defn to-hiccup [markup]
-  (-> markup
-      hiccup/parse
-      hiccup/as-hiccup
-      first
-      last
-      last
-      (update-in [1] #(-> % (dissoc :viewbox) (assoc :viewBox (:viewbox %))))))
-
-(defn ensure-dir! [^String dir]
-  (.mkdirs (io/file dir)))
-
-(defn ensure-parent-dir! [^String path]
-  (ensure-dir! (.getParent (io/file path))))
-
-(defn convert-icon [base-dir out-dir path]
-  (let [target (str out-dir "/" (str/replace path #"\.svg$" ".edn"))
-        hiccup (cond-> (to-hiccup (slurp (str base-dir "/" path)))
-                 (re-find #"fill\/" path) (assoc-in [1 :fill] "currentColor"))]
-    (ensure-parent-dir! target)
-    (spit target (str/replace (pr-str hiccup) #":opacity \"0.2\"" ":opacity \"0.2\" :fill \"currentColor\""))))
-
-(defn convert-icons [base-dir out-dir]
-  (doseq [file (->> (file-seq (io/file base-dir))
-                    (map #(.getPath %))
-                    (filter #(re-find #"\.svg$" %))
-                    (map #(str/replace % (str base-dir "/") "")))]
-    (convert-icon base-dir out-dir file)))
-
-(defn -main [base-dir]
-  (convert-icons base-dir (str "resources/" base-path)))
