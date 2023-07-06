8faa55b28736aa75b0a7942d990be329a23a25e3
Christian Johansen <christian@cjohansen.no>
2023-07-06 14:22:16 +0200

    Make sure icons take default color as expected

--- src/phosphor/icons.cljs
+++ src/phosphor/icons.cljs
@@ -11,12 +11,14 @@
 
 (defn render [id & [{:keys [size color style] :as attrs}]]
   (if-let [svg (get @icons id)]
-    (-> svg
-        (assoc-in [1 :style] (cond-> {:display "inline-block"
-                                      :line-height "1"}
-                               size (assoc :height size)
-                               size (assoc :width size)
-                               color (assoc :color color :fill color)
-                               style (into style)))
-        (update 1 merge (dissoc attrs :size :color :style)))
+    (let [color (or color "currentColor")]
+      (-> svg
+          (assoc-in [1 :style] (cond-> {:display "inline-block"
+                                        :line-height "1"
+                                        :color color
+                                        :fill color}
+                                 size (assoc :height size)
+                                 size (assoc :width size)
+                                 style (into style)))
+          (update 1 merge (dissoc attrs :size :color :style))))
     (throw (js/Error. (str "Icon " id " is not loaded. Try loading it with `load-icon!`, or check that it exists.")))))
